14df9e25a2aec50cb02eef97ea8910631b2723e0
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-01-26 11:18:36 +0900

    setting template

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+import styles from 'app.module.css';
+
+const App = (props) => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App;
