716121a180532d921ee439979018c2af74830293
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-01-26 12:17:51 +0900

    add youtube service & add axios library

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import '@fortawesome/fontawesome-free/js/all.js';
+import axios from 'axios';
+import Youtube from './service/youtube';
+
+const httpClient = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {key: process.env.REACT_APP_YOUTUBE_API_KEY},
+});
+
+const youtube = new Youtube(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/service/youtube.js
@@ -0,0 +1,41 @@
+class youtube  {
+    constructor(httpClient) {
+        this.youtube = httpClient;
+    }
+
+    async mostPopular() {
+        const response = await this.youtube.get('videos', {
+            params: {
+                part: 'snippet',
+                chart: 'mostPopular',
+                maxResults: 25,
+            }
+        });
+        return response.data.items;
+    }
+
+    async search(query) {
+        const response = await this.youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 25,
+                q: query,
+                type: 'video',
+            }
+        });
+
+        return response.data.items.map(item => ({...item, id: item.id.videoId}));
+    }
+
+    async channel(id) {
+        const response = await this.youtube.get('channels', {
+            params: {
+                part: 'snippet',
+                id: id,
+            }
+        });
+
+        return response.data.items[0].snippet.thumbnails.medium.url;
+    }
+
+}
